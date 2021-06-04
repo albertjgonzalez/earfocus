5b07905e444645bc68eacde52a0ff5784f0fba55
albertjgonzalez <ajgonzalez1114@gmail.com>
2021-06-04 11:53:56 -0400

    removed hello world template and tested audio output

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -30,7 +30,7 @@ void EarFocusAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("EarFocus", getLocalBounds(), juce::Justification::topLeft, 1);
 }
 
 void EarFocusAudioProcessorEditor::resized()
--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -150,12 +150,12 @@ void EarFocusAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
     // the samples and the outer loop is handling the channels.
     // Alternatively, you can process the samples with the channels
     // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
+//    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+//    {
+//        auto* channelData = buffer.getWritePointer (channel);
+//
+//        // ..do something to the data...
+//    }
 }
 
 //==============================================================================
