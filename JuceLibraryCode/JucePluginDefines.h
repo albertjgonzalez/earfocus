c4666349de109413d3782cfed8598e9f756c75a8
albertjgonzalez <ajgonzalez1114@gmail.com>
2021-06-04 11:10:44 -0400

    created repo

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -56,7 +56,7 @@
  #define JucePlugin_ManufacturerCode       0x4d616e75
 #endif
 #ifndef  JucePlugin_PluginCode
- #define JucePlugin_PluginCode             0x46636666
+ #define JucePlugin_PluginCode             0x44677632
 #endif
 #ifndef  JucePlugin_IsSynth
  #define JucePlugin_IsSynth                0
