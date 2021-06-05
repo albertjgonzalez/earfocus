71c851b49bfb0ef40ad5b4f5aca1f5622b51d8c6
albertjgonzalez <ajgonzalez1114@gmail.com>
2021-06-04 20:50:31 -0400

    created gain slider

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,6 +16,15 @@ EarFocusAudioProcessorEditor::EarFocusAudioProcessorEditor (EarFocusAudioProcess
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
+    
+    
+    gainSlider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+    gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 100, 25);
+    gainSlider.setRange(0.0, 1.0);
+    gainSlider.setValue(0.5);
+    gainSlider.addListener(this);
+    addAndMakeVisible(gainSlider);
+    
 }
 
 EarFocusAudioProcessorEditor::~EarFocusAudioProcessorEditor()
@@ -35,6 +44,13 @@ void EarFocusAudioProcessorEditor::paint (juce::Graphics& g)
 
 void EarFocusAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    gainSlider.setBounds(getLocalBounds());
+}
+
+void EarFocusAudioProcessorEditor::sliderValueChanged (juce::Slider *slider)
+{
+    if (slider == &gainSlider)
+    {
+        audioProcessor.rawVolume = gainSlider.getValue();
+    }
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -14,20 +14,24 @@
 //==============================================================================
 /**
 */
-class EarFocusAudioProcessorEditor  : public juce::AudioProcessorEditor
+class EarFocusAudioProcessorEditor  : public juce::AudioProcessorEditor,
+                                      public juce::Slider::Listener
 {
 public:
     EarFocusAudioProcessorEditor (EarFocusAudioProcessor&);
-    ~EarFocusAudioProcessorEditor() override;
+    ~EarFocusAudioProcessorEditor();
 
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    void sliderValueChanged (juce::Slider *slider) override;
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     EarFocusAudioProcessor& audioProcessor;
 
+    juce::Slider gainSlider;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EarFocusAudioProcessorEditor)
 };
